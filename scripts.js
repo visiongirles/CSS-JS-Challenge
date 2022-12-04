6d61af07ecf5ea2b3602951526c67eeb0d434c11
Kate Sychenko <busy.sychenko@gmail.com>
2022-12-04 16:53:14 +0300

    infinite scroll

---
+++ scripts.js
@@ -0,0 +1,70 @@
+import { apiKey } from './key.js';
+
+const app = document.getElementById('root');
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.append(container);
+
+const url = `https://api.thecatapi.com/v1/images/search?limit=3&api_key=${apiKey}`;
+
+function fetchData() {
+    showLoader();
+    fetch(url)
+    .then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    loadImages(data);
+})
+}
+
+// fetch(url)
+// .then(function(response) {
+//     return response.json();
+// })
+// .then(function(data) {
+//     loadImages(data);
+// })
+
+
+
+function loadImages(data) {
+        data.forEach(catElement => {
+            console.log(catElement.url);
+
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card'); 
+
+
+        const id = document.createElement('h2');
+        id.textContent = catElement.id;
+
+        const image = document.createElement('img');
+        image.setAttribute('class', 'image');
+        image.src = catElement.url;
+
+        container.appendChild(card);
+        card.appendChild(id);
+        card.appendChild(image);
+        });
+        hideLoader();
+}
+
+
+window.addEventListener('scroll', ()=> {
+    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+        fetchData();
+    }
+})
+const loader = document.querySelector('.loader');
+
+const hideLoader = () => {
+    loader.classList.remove('show');
+};
+ 
+const showLoader = () => {
+    loader.classList.add('show');
+};
+
